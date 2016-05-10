0b194e821881311f0453923ea07337acdca31495
Massimo <mdsiboldi@gmail.com>
2016-05-10 02:18:11 -0700

    added style sheet, changed the way text is displayed for a test.

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
 var WordScreen = React.createClass({
+   genView: function(idx) {
+      return {
+         before: [this.props.words[idx - 3],
+                  this.props.words[idx - 2],
+                  this.props.words[idx - 1]
+                 ],
+         focus: this.props.words[idx],
+         after:  [this.props.words[idx + 1],
+                  this.props.words[idx + 2],
+                  this.props.words[idx + 3]
+                 ]
+      }
+   },
    render: function() {
-      console.log(this.props);
+      var currentWords = this.genView(this.props.wordIdx);
       return (
          <div className="test">
-            {this.props.words.join(' ')}
+            <span id="word-screen-before">{currentWords.before.join(' ')}</span> <span id="word-screen-focus">{currentWords.focus}</span> <span id="word-screen-after"> {currentWords.after.join(' ')}</span>
          </div>
       );
    }
@@ -39,7 +52,7 @@ var TypeArea = React.createClass({
          }
       }
       else {
-         this.props.updateStreak(false);
+         this.props.updateStreak(false, true);
       }
    },
    render: function() {
@@ -61,17 +74,24 @@ var Application = React.createClass({
          streak: 0,
          hiscore: {streak: 0, timeStart: 0, timeEnd:  0},
          timeStart: 0,
+         strokes: 0,
+         mistakes: 0
+
       }
    },
-   updateStreak: function(isGood) {
+   updateStreak: function(isGood, isBackspace) {
       if (this.state.timeStart == 0) this.state.timeStart = Date.now();
       if (isGood) {
+         this.state.strokes++;
          this.state.streak++;
          if (this.state.streak > this.state.hiscore.streak) {
             this.state.hiscore =  {streak: this.state.streak, timeStart: this.state.timeStart, timeEnd: Date.now()};
          }
       }
       else {
+         if (!isBackspace) {
+            this.state.mistakes++;
+         }
          this.state.streak = 0;
          this.state.timeStart = new Date;
       }
@@ -100,13 +120,13 @@ var Application = React.createClass({
          type: 'POST',
          headers: { 'Api-User-Agent': 'supertyper/1.0 (https://example.org/supertyper/; mdsiboldi@gmail.com)', 'Origin': 'http://massimo.cool' },
          success: function(data) {
-            var words = data.query.pages[Object.keys(data.query.pages)[0]].extract.split(/\s/).slice(0, 50);
+            var words = data.query.pages[Object.keys(data.query.pages)[0]].extract.split(/\s/);
             this.setState({words: words, wordIdx: 0, streak: 0});
          }.bind(this)
       });
    },
    nextWord: function() {
-      this.state.words.shift();
+      this.state.wordIdx++;
       this.state.words.push(this.generateNonsense(this.state.keys, 1));
       this.setState(this.state);
    },
@@ -114,7 +134,7 @@ var Application = React.createClass({
       return (
          <div>
             <h1>The App</h1> 
-            <WordScreen words={this.state.words} />
+            <WordScreen words={this.state.words} wordIdx={this.state.wordIdx} />
             <TypeArea currentWord={this.state.words[this.state.wordIdx]}
                nextWord={this.nextWord} 
                streak={this.state.streak} 
@@ -122,6 +142,7 @@ var Application = React.createClass({
             />
             <p style={{fontSize: (12 + this.state.streak / 5) + 'px'}}>{this.state.streak}</p>
             <h2>TOP SCORE: {this.state.hiscore.streak}, {this.state.hiscore.streak * 1000 * 60 / (this.state.hiscore.timeEnd - this.state.hiscore.timeStart)}</h2>
+            <h2>{this.state.strokes * 100 / (this.state.strokes + this.state.mistakes)}</h2>
          </div>
       )
    }
