c50ae5361516c6240608ee8361670055c185034b
Massimo Siboldi <mdsiboldi@gmail.com>
2016-05-30 20:44:08 -0700

    more tests

--- test/components/WordScreen_spec.jsx
+++ test/components/WordScreen_spec.jsx
@@ -8,7 +8,7 @@ import {expect} from 'chai';
 import WordScreen from '../../src/components/WordScreen';
 
 describe('WordScreen', () => {
-  it('puts the lotion on the skin', () => {
+  it('renders first word and surrounding words correctly', () => {
     const component = renderIntoDocument(
       <WordScreen
         words={
@@ -17,14 +17,55 @@ describe('WordScreen', () => {
         wordIdx={0}
       />
     );
-    console.log(document.querySelectorAll('div'));
-    const beforeDiv = findRenderedDOMComponentWithClass(component, 'word-screen-before');
-    const focusDiv = findRenderedDOMComponentWithClass(component, 'word-screen-focus');
-    const afterDiv = findRenderedDOMComponentWithClass(component, 'word-screen-after');
+    const beforeDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-before');
+    const focusDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-focus');
+    const afterDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-after');
 
-    console.log('look',beforeDiv.textContent);
     expect(beforeDiv.textContent).to.equal('');
     expect(focusDiv.textContent).to.equal('this');
     expect(afterDiv.textContent).to.equal('is a cool');
+  });
+  it('renders middle word and surrounding words correctly', () => {
+    const component = renderIntoDocument(
+      <WordScreen
+        words={
+          ['this', 'is', 'a', 'cool', 'and', 'radical', 'test', 'yeah']
+        }
+        wordIdx={4}
+      />
+    );
+    const beforeDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-before');
+    const focusDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-focus');
+    const afterDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-after');
+
+    expect(beforeDiv.textContent).to.equal('is a cool');
+    expect(focusDiv.textContent).to.equal('and');
+    expect(afterDiv.textContent).to.equal('radical test yeah');
+  });
+  it('renders last word and surrounding words correctly', () => {
+    const component = renderIntoDocument(
+      <WordScreen
+        words={
+          ['this', 'is', 'a', 'cool', 'and', 'radical', 'test', 'yeah']
+        }
+        wordIdx={7}
+      />
+    );
+    const beforeDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-before');
+    const focusDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-focus');
+    const afterDiv =
+      findRenderedDOMComponentWithClass(component, 'word-screen-after');
+
+    expect(beforeDiv.textContent).to.equal('and radical test');
+    expect(focusDiv.textContent).to.equal('yeah');
+    expect(afterDiv.textContent).to.equal('');
   });
 });
